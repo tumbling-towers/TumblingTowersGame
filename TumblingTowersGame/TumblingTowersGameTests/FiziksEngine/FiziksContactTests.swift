fd7e95a253ef8bc6413ecdc899fa89e8869db529
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2023-03-19 10:48:53 +0800

    feat: add tests for fiziksengine

---
+++ TumblingTowersGame/TumblingTowersGameTests/FiziksEngine/FiziksContactTests.swift
@@ -0,0 +1,16 @@
+//
+//  FiziksContactTests.swift
+//  TumblingTowersGameTests
+//
+//  Created by Taufiq Abdul Rahman on 19/3/23.
+//
+
+import XCTest
+
+final class FiziksContactTests: XCTestCase {
+    func testConstruct() {
+        let contact = FiziksContact(bodyA: MockFiziksBody(), bodyB: MockFiziksBody(), contactPoint: .zero, collisionImpulse: .zero, contactNormal: .zero)
+        
+        XCTAssertNotNil(contact)
+    }
+}
