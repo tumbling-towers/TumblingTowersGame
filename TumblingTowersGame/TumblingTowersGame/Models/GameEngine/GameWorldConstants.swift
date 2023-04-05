2a14244731f424f842b5e46c9d0a0fab4854163c
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2023-04-05 14:51:37 +0800

    :bug: fix: powerup line position

--- TumblingTowersGame/TumblingTowersGame/Models/GameEngine/GameWorldConstants.swift
+++ TumblingTowersGame/TumblingTowersGame/Models/GameEngine/GameWorldConstants.swift
@@ -16,7 +16,7 @@ struct GameWorldConstants {
 
     static let defaultPowerupHeightStep: Double = 50
 
-    static let defaultInitialPowerupHeight: Double = 20
+    static let defaultInitialPowerupHeight: Double = 200
 
     static let defaultPowerupLineDimensions = CGSize(width: 400, height: 5)
     
